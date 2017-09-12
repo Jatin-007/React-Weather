14b18cd79b6862f7bbaac5fc75b41be1c389f46d
Jatin Suneja <jatinsuneja1903@gmail.com>
2017-09-12 14:42:25 -0700

    Updated Examples.jsx file

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,11 @@ var React = require('react');
 
 var Examples = (props) => {
  return (
-            <h2>Examples Component</h2>
+     <div>
+        <h2>Examples Component!!</h2>
+        <p>This is Examples Component</p>
+     </div>
+         
         )
 }
 
